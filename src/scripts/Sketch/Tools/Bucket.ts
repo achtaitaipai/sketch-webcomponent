d4965cadf2400ff52b4106a8abc995e0195a7405
achtaitaipai <charles.cailleteau@gmail.com>
2022-06-10 16:34:10 +0200

    fix bucket not working on first column and first row

--- src/scripts/Sketch/Tools/Bucket.ts
+++ src/scripts/Sketch/Tools/Bucket.ts
@@ -4,7 +4,7 @@ import Tool from './AbstractTool'
 export default class Bucket extends Tool {
 	public click(e: Coordinate): void {
 		const coord = this._sketch.gridCoordinate(e)
-		if (coord.x > 0 && coord.y > 0 && coord.x < this._sketch.width && coord.y < this._sketch.height) {
+		if (coord.x >= 0 && coord.y >= 0 && coord.x < this._sketch.width && coord.y < this._sketch.height) {
 			this.drawing?.bucket(coord, this._sketch.color)
 			this._sketch.updatePreview()
 		}
